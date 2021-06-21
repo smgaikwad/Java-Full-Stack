b7e28c79b92fe70b665878a1b2f7d3985232ead3
Suraj Gaikwad <83628499+smgaikwad@users.noreply.github.com>
2021-06-21 10:47:05 +0530

    OOP programs added

---
+++ encapsulation/EncapsulationDemo.java
@@ -0,0 +1,8 @@
+package encapsulation;
+
+public class EncapsulationDemo {
+    public static void main(String[] args) {
+        Student sooraj = new Student(1,1234,"Sooraj Gaikwad", "Computer Engineering");
+        sooraj.setRollNo(-1);
+    }
+}
